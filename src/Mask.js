c6298de7d0860d78e7b76b9baf309f6c4a3f93e3
Gabriel Michalski <gmichalski@unimedsc.com.br>
2017-04-28 11:01:56 -0300

    most unit tests

--- src/Mask.js
+++ src/Mask.js
@@ -1,13 +1,26 @@
-import config from './locale/pt-BR';
-
 class Mask {
 
-  constructor() {
+  constructor(config) {
     this.config = config;
   }
 
-  build(text, format, defaultValue = '') {
-    const regexp = this.config[format];
+  exec(text, mask, placeholder) {
+    const chars = mask.split('');
+    const regexp = [];
+
+    for (let index = 0; index < chars.length; index += 1) {
+      switch (chars[index]) {
+        case '#' : regexp.push(/\d/); break;
+        case 'A' : regexp.push(/[a-z]/i); break;
+        case 'N' : regexp.push(/[a-z0-9]/i); break;
+        default : regexp.push(chars[index]); break;
+      }
+    }
+    return this.build(text, regexp, placeholder);
+  }
+
+  build(text, mask, placeholder = '', debug = false) {
+    const regexp = Array.isArray(mask) ? mask : this.config[mask];
     const chars = text.split('');
     const formated = [];
 
@@ -19,9 +32,9 @@ class Mask {
         } else if (chars[indexChars]) {
           index -= 1;
         } else {
-          formated.push(defaultValue);
+          formated.push(placeholder);
         }
-      } else if (chars[indexChars + 1] || defaultValue) {
+      } else if (chars[indexChars] || placeholder) {
         formated.push(regexp[index]);
         indexChars -= 1;
       }
